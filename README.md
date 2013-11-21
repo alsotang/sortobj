07fd435384f5b2197b49666c9193118dff3fabe6
Alsotang <alsotang@gmail.com>
2013-11-21 21:18:12 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# sortobj
+
+Hello world!
---
+++ test/sortobj.test.js
@@ -0,0 +1,7 @@
+var should = require('chai').should();
+
+describe('sortobj.test.js', function () {
+  it('should be ok', function () {
+    true.should.be.true;
+  });
+});
